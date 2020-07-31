a641f75a1cee488b5c664981ea43a6aa0441a02b
peiran <271807119@qq.com>
2020-07-31 17:31:13 +0800

    learn array and pointer

---
+++ chapter_10/no_data.c
@@ -0,0 +1,17 @@
+// no_data.c -- 初始化数组失败会怎样？
+#include <stdio.h>
+#define SIZE 4
+
+int main(void)
+{
+    int no_data[SIZE]; // 未初始化数组-- 数组未初始化导致数据不可预估
+    int i;
+
+    printf("%2s%14s\n", "i", "no_data[i]");
+    for (i = 0; i < SIZE; i++)
+    {
+        printf("%2d%14d\n", i, no_data[i]);
+    }
+
+    return 0;
+}
